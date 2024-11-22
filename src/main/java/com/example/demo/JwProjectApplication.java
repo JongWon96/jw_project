d9237425b24f0a88d30954503fdcb7e1063d2c16
JongWon <sily714@naver.com>
2024-11-22 15:30:58 +0900

    create project

---
+++ src/main/java/com/example/demo/JwProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwProjectApplication.class, args);
+	}
+
+}
